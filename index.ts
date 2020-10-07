18f880afa6c602f2f03a55491f0e0646a37c837b
Anwesh43 <anweshthecool0@gmail.com>
2020-10-07 16:41:29 +0530

    adding DrawingUtill class

--- index.ts
+++ index.ts
@@ -28,3 +28,45 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSailPath(context : CanvasRenderingContext2D, sf3 : number, size : number) {
+        context.save()
+        context.beginPath()
+        context.moveTo(0, 0)
+        context.lineTo(size, h / 2)
+        context.lineTo(size, h)
+        context.lineTo(0, h)
+        context.lineTo(0, 0)
+        context.clip()
+        context.fillRect(0, 0, w, h * sf3)
+        context.restore()
+    } 
+
+    static drawSailShapeFill(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const size : number = w / sizeFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        DrawingUtil.drawLine(context, 0, 0, size * sf1, h * 0.5 * sf1)
+        DrawingUtil.drawLine(context, size, h / 2, size, h / 2 * (1 + sf2))
+        DrawingUtil.drawSailPath(context, sf3, size)        
+    }
+
+    static drawSSFNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawSailShapeFill(context, i, scale)
+    }
+}
